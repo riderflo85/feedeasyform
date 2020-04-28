987a770026afb4e7b04a8f471cbbb9313b53763f
Florent <grenaille.florent85@gmail.com>
2020-04-28 14:44:58 +0200

    Add models

--- food/models.py
+++ food/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=45)
+
+
+class Food(models.Model):
+    name = models.CharField(max_length=45)
+    description = models.TextField()
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)  # new table
+
--- planning/models.py
+++ planning/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from food.models import Food
 
-# Create your models here.
+
+class FoodQuantity(models.Model):
+    quantity = models.CharField(max_length=45)
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+
+
+class Categorie(models.Model):
+    name = models.CharField(max_length=45)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=45)
+    prepare_duration = models.CharField(max_length=45)
+    cooking_time = models.CharField(max_length=45)
+    step = models.TextField()
+    food_quantity = models.ManyToManyField(FoodQuantity)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+    price_scale = models.CharField(max_length=45)
+    level = models.CharField(max_length=45)
