af1c14c02ca8562c4c4dcb9fa107921f4f959e4e
Florent <grenaille.florent85@gmail.com>
2020-04-29 15:33:14 +0200

    Add view and template 'databases' for the user look the databases

--- planning/urls.py
+++ planning/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "planning"
 urlpatterns = [
     path('new_recipe/', views.create_recipe, name='new_recipe'),
+    path('databases/', views.show_and_update_db, name='databases'),
 ]
--- planning/views.py
+++ planning/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import RecipeForm, CategorieForm
-from .models import Recipe
+from food.models import Food, Group
+from .models import Recipe, Categorie
 
 
 @login_required
@@ -28,3 +29,20 @@ def create_recipe(request):
             'form_categ': form_categorie
         }
         return render(request, 'planning/new_recipe.html', context)
+
+
+@login_required
+def show_and_update_db(request):
+    foods = Food.objects.all()
+    food_group = Group.objects.all()
+    recipes = Recipe.objects.all()
+    recipe_categ = Categorie.objects.all()
+
+    context = {
+        'food': foods,
+        'food_group': food_group,
+        'recipe': recipes,
+        'recipe_categ': recipe_categ
+    }
+
+    return render(request, 'planning/databases.html', context)
