d321c84d168491b854306cda5cb134481c08e07c
Florent <grenaille.florent85@gmail.com>
2020-12-11 16:22:35 +0100

    Updated the search food form

--- user/static/user/js/script.js
+++ user/static/user/js/script.js
@@ -40,15 +40,53 @@ function displayResult(foods) {
 
 function displayQuantityForm(foods) {
     const parentNode = $("#quantityFormBloc");
+    let inputFields = [];
 
     for (const food of foods) {
         const formatedFood = food.split(':');
         const newForm = `<label for="food-${formatedFood[0]}">${formatedFood[1]}</label>\n
-        <input type="text" class="form-control mb-3" name="quantity" id="food-${formatedFood[0]}">`;
+        <input type="text" class="form-control mb-3" name="quantity" id="food-${formatedFood[0]}" data-id="${formatedFood[0]}">`;
 
         $(newForm).appendTo(parentNode);
+        inputFields.push({
+            label: formatedFood[1],
+            input: $(`#food-${formatedFood[0]}`)
+        });
     }
+    parentNode.slideDown(500);
 
+    return inputFields;
+}
+
+
+function displayFoodsAndQuantity(parentNode, fieldsInput) {
+    for (const field of fieldsInput) {
+        const parsedData = field.input.val().split(';');
+        const item = `
+        <tr id="${field.input.data('id')}" data-id="${field.input.data('id')}">
+            <td>${field.label}</td>
+            <td>${parsedData[0].trim()}</td>
+            <td>${parsedData[1].trim()}</td>
+            <td>
+                <button class="btn btn-danger btn-sm" id="clearFoodTable-${field.input.data('id')}">
+                    <i class="fas fa-minus"></i>
+                </button>
+            </td>
+        </tr>`;
+
+        $(item).appendTo(parentNode);
+        field.input.attr('disabled', 'true');
+
+        const btnClearFoodTable = $(`#clearFoodTable-${field.input.data('id')}`);
+
+        btnClearFoodTable.on('click', () => {
+            const thisField = $(`#${field.input.data('id')}`);
+            thisField.fadeOut(500, () => {
+                thisField.remove();
+            }); 
+        });
+    }
+    parentNode.fadeIn(500);
 }
 
 
@@ -65,10 +103,14 @@ $(document).ready(() => {
     const quantityFormBloc = $("#quantityFormBloc");
     const selectArea = $("#foodsResult");
     const fomrQuantity = $("#quantityFormBloc");
+    let quantityInput;
 
 
     selectArea.focus(() => {
         btnAddFood.removeAttr('disabled');
+        fomrQuantity.fadeOut(500, () => {
+            fomrQuantity.empty();
+        });
     });
 
 
@@ -79,6 +121,13 @@ $(document).ready(() => {
         }
     });
 
+    
+    inputSearch.focus(() => {
+        fomrQuantity.fadeOut(500, () => {
+            fomrQuantity.empty();
+        });
+    });
+
 
     inputSearch.blur(() => {
         inputSearch.val("");
@@ -86,16 +135,27 @@ $(document).ready(() => {
 
 
     btnAddFood.on('click', () => {
-        displayQuantityForm(selectArea.val());
+        quantityInput = displayQuantityForm(selectArea.val());
         btnAddFood.attr('disabled', 'true');
         btnValideFormQuantity.removeAttr('disabled');
     });
 
 
     btnClearFoodSelected.on('click', () => {
-        listingFood.empty();
-        fomrQuantity.empty();
-        selectArea.empty();
+        fomrQuantity.fadeOut(500, () => {
+            fomrQuantity.empty();
+            selectArea.empty();
+        });
+        listingFood.fadeOut(500, () => {
+            listingFood.empty();
+        });
+    });
+
+
+    btnValideFormQuantity.on('click', (event) => {
+        displayFoodsAndQuantity(listingFood, quantityInput);
+        btnAddFood.attr('disabled', 'true');
+        $(event.target).attr('disabled', 'true');
     });
 
 });
