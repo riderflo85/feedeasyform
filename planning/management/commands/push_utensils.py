b1125c571534fe69524ef6d8dcce1e07ac7fd10e
Florent (Mac) <grenaille.florent85@gmail.com>
2020-12-17 16:44:43 +0100

    Try to bug fix with the management commands

--- planning/management/commands/push_utensils.py
+++ planning/management/commands/push_utensils.py
@@ -20,24 +20,24 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         directory = os.path.join(
-            os.path.dirname(__file__),
+            os.path.dirname(os.path.abspath(__file__)),
             '../../../feedeasyform_project/assets/'
         )
         csvfile_path = directory + 'table_ustensils_cuisine.csv'
 
-        try:
-            with open(csvfile_path, newline='') as csvfile:
-                utensils = csv.DictReader(csvfile)
+#         try:
+        with open(csvfile_path, newline='') as csvfile:
+            utensils = csv.DictReader(csvfile)
 
-                for line in utensils:
-                    utensil = Utensil(name=line['Ustensils cuisine'])
-                    utensil.save()
+            for line in utensils:
+                utensil = Utensil(name=line['Ustensils cuisine'])
+                utensil.save()
 
-                    if kwargs['verbose']:
-                        self.stdout.write(
-                            f"{self.style.SUCCESS(utensil)} ajouté à la base de données"
-                        )
+                if kwargs['verbose']:
+                    self.stdout.write(
+                        f"{self.style.SUCCESS(utensil)} ajouté à la base de données"
+                    )
 
-        except:
-            self.stderr.write(self.style.ERROR('Une erreur est survenu. \
-\nIl est possible que les ustensils existe déjà dans la base de données.'))
+#         except:
+#             self.stderr.write(self.style.ERROR('Une erreur est survenu. \
+# \nIl est possible que les ustensils existe déjà dans la base de données.'))
