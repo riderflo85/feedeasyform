5ea16b68ef8315376e9d8770d1f8956f6dc5cc0f
Florent <grenaille.florent85@gmail.com>
2021-01-14 10:51:05 +0100

    Bug fix in the urls project

--- feedeasyform_project/urls.py
+++ feedeasyform_project/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT)
