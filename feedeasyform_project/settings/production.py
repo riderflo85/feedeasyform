088b0d3f4d34d66c4ad10f8ba24ce29b54194ff0
Florent <grenaille.florent85@gmail.com>
2021-01-14 11:36:46 +0100

    Updated prod conf

--- feedeasyform_project/settings/production.py
+++ feedeasyform_project/settings/production.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['feedeasyform.herokuapp.com']
+ALLOWED_HOSTS = ['feedeasyform.herokuapp.com', 'feed-easy.fr']
 
 MIDDLEWARE.append('whitenoise.middleware.WhiteNoiseMiddleware')
 
