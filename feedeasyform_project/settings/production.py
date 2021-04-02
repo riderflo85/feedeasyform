8218d0db87c57d1a07179f11f65c01a4c835a80b
Florent (Mac) <grenaille.florent85@gmail.com>
2021-04-02 09:43:54 +0200

    Added the sentry sdk in the prod config

--- feedeasyform_project/settings/production.py
+++ feedeasyform_project/settings/production.py
@@ -1,4 +1,7 @@
 import os
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from .default import *
 
 
@@ -32,5 +35,19 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+)
+
+sentry_sdk.init(
+    dsn="https://e88ae8aa21d84c2f90035f52ac66b507@o279273.ingest.sentry.io/5703372",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
 )
 
